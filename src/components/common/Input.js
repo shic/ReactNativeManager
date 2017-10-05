76f7fbfe1f0567d782618542317c3e0cd0805c04
shi <itshilei@gmail.com>
2017-10-05 17:46:18 +0200

    onChangeText！！！

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { TextInput, View, Text } from 'react-native';
 
-const Input = ({ label, value, onTextChange, placeholder, secureTextEntry }) => {
+const Input = ({ label, value, onChangeText, placeholder, secureTextEntry }) => {
   const { inputStyle, labelStyle, containerStyle } = styles;
 
   return (
@@ -13,7 +13,7 @@ const Input = ({ label, value, onTextChange, placeholder, secureTextEntry }) => 
           autoCorrect={false}
           style={inputStyle}
           value={value}
-          onTextChange={onTextChange}
+          onChangeText={onChangeText}
       />
     </View>
   );
